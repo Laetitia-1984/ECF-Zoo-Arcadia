96b5054f859de86054a2e4f7b5a13966b2292add
Laetitia Bouslah <laetitia.bouslah@free.fr>
2024-02-10 08:10:54 +0100

    Ajout systeme routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,11 @@
+import Route from "./Route.js";
+
+//Définir ici les routes
+export const allRoutes = [
+    new Route ("/", "Accueil", "/pages/home.html"),
+    new Route ("/habitats", "Habitats", "/pages/habitats.html"),
+];
+
+
+//Le titre s'affiche comme ceci: Route.titre - websitename
+export const websiteName = "Zoo Arcadia";
